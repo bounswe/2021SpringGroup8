605d8ae5d4d37d365e736b8c5adae8a3781b728e
mcavus <muhammet.cavus@boun.edu.tr>
2021-06-06 16:28:18 +0300

    first commit

--- practice-app/assignmentM2/apis/models.py
+++ practice-app/assignmentM2/apis/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models import CharField
+from django.urls import reverse
+import requests
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    city_name = models.CharField(max_length=20,default='Istanbul')
+    
+
+    def get_absolute_url(self):
+        return reverse("events:event-detail", kwargs={"id": self.id})
+
+
