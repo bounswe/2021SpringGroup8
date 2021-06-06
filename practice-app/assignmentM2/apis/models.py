0c555b7a02000d86932eee7add5d607cfaf3cfd3
mcavus <muhammet.cavus@boun.edu.tr>
2021-06-06 16:28:18 +0300

    first commit

--- practice-app/assignmentM2/apis/models.py
+++ practice-app/assignmentM2/apis/models.py
@@ -1,5 +1,20 @@
 from django.db import models
+from django.db.models import CharField
+from django.urls import reverse
+import requests
 
+
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    city_name = models.CharField(max_length=20,default='Istanbul')
+    
+
+    def get_absolute_url(self):
+        return reverse("events:event-detail", kwargs={"id": self.id})
+
+
+<<<<<<< HEAD
 # Create your models here.
 
 
@@ -8,3 +23,5 @@ class Meal(models.Model):
     category = models.CharField(max_length=50)
     area = models.CharField(max_length=50)
     recipe = models.TextField()
+=======
+>>>>>>> first commit
