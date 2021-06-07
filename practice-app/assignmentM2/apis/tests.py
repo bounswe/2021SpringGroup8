9ba3a1a8d533791724969a4cdce4b14c79665e96
uysalGokhan <56603776+uysalGokhan@users.noreply.github.com>
2021-06-07 13:37:31 +0300

    unit test for getmealrecipe is added

--- practice-app/assignmentM2/apis/tests.py
+++ practice-app/assignmentM2/apis/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import json
+from django.http.response import HttpResponse
+from django.test import TestCase, Client, testcases
+from django.urls import reverse
 
-# Create your tests here.
+# initialize the APIClient app
+client = Client()
+
+
+class MealRecipeTests(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_default_connection(self):
+        response: HttpResponse = client.get(reverse('getmealrecipe'))
+        content = response.content.decode()
+        self.assertGreaterEqual(len(content), 10)
