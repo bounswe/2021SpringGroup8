87690011f9a931cbfacc9f06a8aebd9fa8045b0a
Akif Faruk Nane <faruknane54@hotmail.com>
2021-06-07 12:05:32 +0300

    Update views.py

--- practice-app/assignmentM2/apis/views.py
+++ practice-app/assignmentM2/apis/views.py
@@ -2,20 +2,37 @@ from typing import List
 from django.http.request import HttpRequest
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import numpy as np
 # Create your views here.
 import urllib
+import io
+import base64
+
+def gethtmlimage(url):
+    from PIL import Image
+    req = urllib.request.Request(url,headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+    })
+
+    im = Image.open(urllib.request.urlopen(req))
+    rawBytes = io.BytesIO()
+    im.save(rawBytes, "PNG")
+    rawBytes.seek(0)  # return to the start of the file
+    basestr = base64.b64encode(rawBytes.read()).decode("utf-8")
+    #print(base64.b64encode(rawBytes.read()), "|||", basestr)
+    ss = """<img src="data:image/png;base64, """ + basestr + """" />"""
+    return ss
 
 def examplepage(request):
     return HttpResponse("<h1>API Example Page!<h1>")
 
 def getflag(request:HttpRequest):
     if request.method == "GET" and "country" in request.GET:
-        return HttpResponse("<img src=\"https://www.countryflags.io/" + request.GET["country"] + "/flat/64.png\">")
+        return HttpResponse(gethtmlimage("https://www.countryflags.io/" + request.GET["country"] + "/flat/64.png"))
     elif request.method == "POST" and "country" in request.POST:
-        return HttpResponse("<img src=\"https://www.countryflags.io/" + request.POST["country"] + "/flat/64.png\">")
+        return HttpResponse(gethtmlimage("https://www.countryflags.io/" + request.POST["country"] + "/flat/64.png"))
 
-    return HttpResponse("""<img src="https://www.countryflags.io/be/flat/64.png">""")
+    return HttpResponse(gethtmlimage("https://www.countryflags.io/be/flat/64.png"))
 
 def getcurrencies(request:HttpRequest):
     url = "https://free.currconv.com/api/v7/convert?q=TRY_USD&compact=ultra&apiKey=55d560f06e174022b414"
