bc7d915607c937ff17ec5fec64ba80effc28c198
uysalGokhan <56603776+uysalGokhan@users.noreply.github.com>
2021-06-09 12:38:20 +0300

    The funcionality of adding data to database for mealrecipes is added

--- practice-app/assignmentM2/apis/models.py
+++ practice-app/assignmentM2/apis/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    area = models.CharField(max_length=50)
+    recipe = models.TextField()
