2a0d9396f253b17ab7b456ba35aa300e5de3cece
Mehmet Selahaddin Şentop <56365256+mssentop@users.noreply.github.com>
2021-06-10 13:31:14 +0300

    Committing files of quote-api
    I am adding the files that are changed during development of quote-api feature.

--- practice-app/assignmentM2/apis/models.py
+++ practice-app/assignmentM2/apis/models.py
@@ -13,10 +13,3 @@ class Event(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     city_name = models.CharField(max_length=20, default='Istanbul')
-
-class User(models.Model):
-    name= models.CharField(max_length=30)
-    username = models.CharField(max_length=30)
-    email = models.CharField(max_length=250)
-    isActive = models.BooleanField(default=True)
-    password = models.CharField(max_length=30)
