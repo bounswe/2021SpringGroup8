5d848bf0cf7ba8da7df95d15f79ce060bb901f21
Mustafa Can Aydin <mustafacanaydin@Mustafas-MacBook-Pro.local>
2021-06-13 18:41:20 +0300

    bug related to environmental variables and tests have been resolved

--- practice-app/assignmentM2/assignmentM2/settings.py
+++ practice-app/assignmentM2/assignmentM2/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import os.path
-
+import environ
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
@@ -22,7 +24,7 @@ SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hjpwmczb)+#nm0l_$jka+#m6y!v%wdbph!dk-jmip393^@s%ru'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
