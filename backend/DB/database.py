4a90f356f2777b9b0965363bd5e46d2d402c68f4
uysalGokhan <56603776+uysalGokhan@users.noreply.github.com>
2021-11-07 13:56:54 +0300

    functions are created without functioanlities

--- backend/DB/database.py
+++ backend/DB/database.py
@@ -5,5 +5,14 @@ db = client["CommunityDB"]
 print(db)
 print(client.list_database_names())
 
+# this function returns true if creating account process is valid otherwise returns false
+def signup(password, username):
+    return True
 
+# this function returns the id of user if the user is not real returns -1
+def signin(password, username):
+    return -1;
 
+# this function returns the sha256 prefix for user's password
+def get_prefix():
+    return 1
