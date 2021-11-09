acc9f1e8455abcf18579e1e9ff2b3848d214bd78
uysalGokhan <56603776+uysalGokhan@users.noreply.github.com>
2021-11-09 17:22:21 +0300

    database functionalities are added

--- backend/DB/database.py
+++ backend/DB/database.py
@@ -2,17 +2,34 @@ import pymongo
 
 client = pymongo.MongoClient("mongodb+srv://admin:admin@cmpe451db.zec58.mongodb.net/Cmpe451DB?retryWrites=true&w=majority")
 db = client["CommunityDB"]
-print(db)
-print(client.list_database_names())
+userCollection = db["users"]
+usercount = 1
+
 
 # this function returns true if creating account process is valid otherwise returns false
-def signup(password, username):
-    return True
+def signup(user_dict):
+    username = user_dict["username"]
+    myquery = { "username": username }
+    mydoc = userCollection.find_one(myquery)
+    if mydoc == None:
+            x = userCollection.insert_one(user_dict)
+            user = userCollection.find_one(myquery)
+            user_return_dict = {"username" : username, "id": user["_id"], "email": user["email"]}
+            return user_return_dict
+    else:
+        return False
+
 
 # this function returns the id of user if the user is not real returns -1
-def signin(password, username):
-    return -1;
+def signin(user_dict):
+    user = userCollection.find_one(user_dict)
+    if user == None :
+        return False
+    else:
+        user_return_dict = {"username" : user["username"], "id": user["_id"], "email": user["email"]}
+        return user_return_dict
 
-# this function returns the sha256 prefix for user's password
-def get_prefix():
-    return 1
+if __name__== "__main__":
+    print(signup({"username": "abaf", "password" : "12345", "email": "abca@gmail.com"}))
+    print(signin({"username": "abaf", "password" : "12345"}))
+    userCollection.drop()
