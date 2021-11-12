9b3589791693d830a7b2e7912b29017ac8cb13d0
uysalGokhan <56603776+uysalGokhan@users.noreply.github.com>
2021-11-12 21:24:21 +0300

    find_user functionality is added

--- backend/DB/database.py
+++ backend/DB/database.py
@@ -18,7 +18,6 @@ class DatabaseManager:
         myquery = { "username": username }
         mydoc = self.userCollection.find_one(myquery)
         if mydoc == None:
-                user_dict["subscribers"] = ""
                 x = self.userCollection.insert_one(user_dict)
                 user = self.userCollection.find_one(myquery)
                 user_return_dict = {"username" : username, "id": str(user["_id"]), "email": user["email"]}
@@ -36,25 +35,24 @@ class DatabaseManager:
             user_return_dict = {"username" : user["username"], "id": str(user["_id"]), "email": user["email"]}
             return user_return_dict
 
+    def find_user(self, userId):
+        user = self.userCollection.find_one({"_id": ObjectId(userId)})
+        user_return_dict = {"username" : user["username"], "id": str(user["_id"]), "email": user["email"]}
+        return user_return_dict
+
     def subscribe_community(self, userId, communityId):
         community = self.communityCollection.find_one({"_id": ObjectId(communityId)})
         user = self.userCollection.find_one({"_id": ObjectId(userId)})
         if (community != None) & (user != None):
-            user_subscribers = user.get("subscribers")
-            if user_subscribers == "":
-                user_subscribers = communityId
-            else:
-                user_subscribers += ";" + communityId   
-            newvalues = { "$set": {"subscribers": user_subscribers}}
-            self.communityCollection.update_one({"_id": ObjectId(userId)}, newvalues)
+            self.userCollection.update_one( 
+            { "_id" : user["_id"] },
+            { "$push": { "subscribers": communityId}}
+            )
 
-            community_subscribers = community.get("subscribers")
-            if community_subscribers == "":
-                community_subscribers = userId
-            else:
-                community_subscribers += ";" + userId   
-            newvalues = { "$set": {"subscribers": community_subscribers}}
-            self.communityCollection.update_one({"_id": ObjectId(communityId)}, newvalues)
+            self.communityCollection.update_one( 
+            { "_id" : community["_id"] },
+            { "$push": { "subscribers": userId}}
+            )
             return True
         else:
             return False
@@ -124,7 +122,7 @@ if __name__== "__main__":
 #    print(dbm.signin({"username": "abaf", "password" : "12345"}))
 #    print(user)
 #    print(community)
-#    print(dbm.subscribe_community(user["id"], community["id"]))
+    print(dbm.subscribe_community(user["id"], community["id"]))
 #    print(dbm.create_post({"postTitle": "post1", "description": "new post1 here",
 #            "creationTime": "12.11.2021", "postedBy": "new user1", "communityId": x["id"]}))
     print(dbm.delete_community(community["id"]))
