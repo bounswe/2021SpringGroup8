26bcd4bcf53f13a6b79993b3d9a3f2dc461bfa83
Mehmet Temizel <mehmet.temizel@boun.edu.tr>
2021-12-23 01:46:22 +0300

    Search bar field is added on communities page, basically

--- frontend/src/public-content/pages/Communities.js
+++ frontend/src/public-content/pages/Communities.js
@@ -1,81 +1,129 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import UserCommunityService from "../../services/user-community.service";
-import {withRouter} from 'react-router-dom';
-import {Button, Card, Col, Container} from "react-bootstrap";
+import { withRouter } from "react-router-dom";
+import { Button, Card, Col, Container } from "react-bootstrap";
+import {Grid} from "@mui/material";
 import Header from "../Components/header/header2";
 import Profilebar from "../../components/profilebar";
+import SearchIcon from "@mui/icons-material/Search";
+import IconButton from "@mui/material/IconButton";
+import InputBase from "@mui/material/InputBase";
 
 class Communities extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      communities: [],
+      activeOption: "Home",
+      searchValue: "",
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            communities: [],
-            activeOption: "Home"
-        }
-    }
+  componentDidMount() {
+    this.retrieveCommunities();
+  }
 
-    componentDidMount() {
-        this.retrieveCommunities();
-    }
+  retrieveCommunities() {
+    UserCommunityService.getCommunities()
+      .then((response) => {
+        this.setState({
+          communities: response.data["@return"],
+        });
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e.toString());
+      });
+  }
 
-    retrieveCommunities() {
-        UserCommunityService.getCommunities()
-            .then(response => {
-                this.setState({
-                    communities: response.data['@return']
-                });
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e.toString());
-            });
-    }
+  goCommunity(id) {
+    console.log(id);
+    this.props.history.push("/community/" + id);
+  }
 
-    goCommunity(id) {
-        console.log(id);
-        this.props.history.push("/community/" + id);
-    }
+  goProfile(id) {
+    console.log(id);
+    this.props.history.push("/profile/" + id);
+  }
 
-    goProfile(id) {
-        console.log(id);
-        this.props.history.push("/profile/" + id);
-    }
+  // text box => onchange = {handleChange}
+  handleChange(event) {
+    this.setState({ searchValue: event.target.value });
+  }
 
-    render() {
-        const {communities} = this.state
+  //button => onClick = {onClick}
+  onClick() {
+    // api call or filter
+    window.alert(this.state.searchValue);
+  }
 
-        return (
-            <>
-                <Profilebar/>
-                <Header/>
-            <Container fluid="md">
-                <header className="jumbotron">
-                    <h3>
-                        <strong>Enjoy various amazing communities</strong>
-                    </h3>
-                </header>
-                {communities && communities.map((community, index) => (
-                    <Col md="9">
-                        <Card style={{width: "100%"}}>
-                            <Card.Img variant="top" src={"https://picsum.photos/" +index +"400/200"} height={200}/>
-                            <Card.Body >
-                                <Card.Title
-                                    onClick={this.goCommunity.bind(this, community.id)}>{community.CommunityTitle}</Card.Title>
-                                <Card.Subtitle onClick={
-                                    this.goProfile.bind(this, community.createdBy.id)}> {community.createdBy.username} </Card.Subtitle>
-                                <Button variant="primary" onClick={this.goCommunity.bind(this, community.id)}>Go to Community</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+  render() {
+    const { communities } = this.state;
 
-                ))}
-            </Container>
-                </>
-        );
-    }
-
+    return (
+      <>
+        <Profilebar />
+        <Header />
+        <Container fluid="md">
+          <Grid item xs={12} md={8}>
+            <InputBase
+              onChange={this.handleChange.bind(this)}
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search Community"
+            />
+            <IconButton
+              type="submit"
+              sx={{ p: "10px" }}
+              aria-label="search"
+              onClick={this.onClick.bind(this)}
+            >
+              <SearchIcon />
+            </IconButton>
+          </Grid>
+          <header className="jumbotron">
+            <h3>
+              <strong>Enjoy various amazing communities</strong>
+            </h3>
+          </header>
+          {communities &&
+            communities.map((community, index) => (
+              <Col md="9">
+                <Card style={{ width: "100%" }}>
+                  <Card.Img
+                    variant="top"
+                    src={"https://picsum.photos/" + index + "400/200"}
+                    height={200}
+                  />
+                  <Card.Body>
+                    <Card.Title
+                      onClick={this.goCommunity.bind(this, community.id)}
+                    >
+                      {community.CommunityTitle}
+                    </Card.Title>
+                    <Card.Subtitle
+                      onClick={this.goProfile.bind(
+                        this,
+                        community.createdBy.id
+                      )}
+                    >
+                      {" "}
+                      {community.createdBy.username}{" "}
+                    </Card.Subtitle>
+                    <Button
+                      variant="primary"
+                      onClick={this.goCommunity.bind(this, community.id)}
+                    >
+                      Go to Community
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+        </Container>
+      </>
+    );
+  }
 }
 
 export default withRouter(Communities);
--- frontend/src/public-content/pages/Community.js
+++ frontend/src/public-content/pages/Community.js
@@ -1,10 +1,10 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import UserCommunityService from "../../services/user-community.service";
 import UserService from "../../services/user.service";
-import AuthService from "../../services/auth.service"
-import {withRouter} from 'react-router-dom';
-import {Avatar, Container, Grid, SpeedDial} from "@mui/material";
+import AuthService from "../../services/auth.service";
+import { withRouter } from "react-router-dom";
+import { Avatar, Container, Grid, SpeedDial } from "@mui/material";
 import CommunityAbout from "../Components/CommunityAbout";
 import CommunityPosts from "../Components/CommunityPosts";
 import Button from "@mui/material/Button";
@@ -15,163 +15,198 @@ import BasicSpeedDial from "../Components/BasicSpeedDial";
 import Profilebar from "../../components/profilebar";
 
 class Community extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      activeOption: "Home",
+      title: "example-title",
+      description: "example-description",
+      creator: "example-creator",
+      subscribers: [],
+      subscribed: false,
+      variant: "contained",
+      text: "Subscribe",
+      owner: false,
+      id: "",
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: [],
-            activeOption: "Home",
-            title: "example-title",
-            description: "example-description",
-            creator: "example-creator",
-            subscribers: [],
-            subscribed: false,
-            variant: 'contained',
-            text: 'Subscribe',
-            owner: false,
-            id: ''
+    };
+    this.subscribe = this.subscribe.bind(this);
+  }
+
+  componentDidMount() {
+    console.log(this.props.match.params.id);
+    this.retrieveCommunity(this.props.match.params.id);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    this.retrieveCommunity();
+  }
+
+  retrieveUsersCommunities() {
+    let users_communities = [];
+    UserService.getUsersCommunities()
+      .then((response) => {
+        if (response.data["@success"] === "True") {
+          let users_communities = response.data["@return"].subscribes;
+          console.log(users_communities);
+        } else {
+          console.log(response.data["@error"]);
         }
-        this.subscribe = this.subscribe.bind(this)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log(users_communities);
+  }
+
+  checkSubscribeStatus() {
+    const myUser = AuthService.getCurrentUser();
+    if (this.state.subscribers.some((user) => user.id === myUser.id)) {
+      /* vendors contains the element we're looking for */
+      console.log("subscribed");
+      this.setState({
+        subscribed: true,
+        variant: "outlined",
+        text: "Unsubscribe",
+      });
+    } else {
+      this.setState({
+        subscribed: false,
+        variant: "contained",
+        text: "Subscribe",
+      });
+      console.log("not subscribed yet");
     }
+  }
 
-
-    componentDidMount() {
-        console.log(this.props.match.params.id);
-        this.retrieveCommunity(this.props.match.params.id);
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        this.retrieveCommunity()
-    }
-
-    retrieveUsersCommunities() {
-        let users_communities = []
-        UserService.getUsersCommunities().then(response => {
-            if (response.data['@success'] === "True") {
-                let users_communities = response.data['@return'].subscribes
-                console.log(users_communities)
-            } else {
-                console.log(response.data['@error'])
-            }
-        }).catch(error => {
-            console.log(error)
+  subscribe() {
+    console.log(this.state);
+    if (this.state.subscribed) {
+      userCommunityService
+        .unsubscribeCommunity(this.props.match.params.id)
+        .then((response) => {
+          console.log(response);
+          if (response.data["@success"] === "True") {
+            this.setState({
+              subscribed: false,
+              variant: "contained",
+              text: "Subscribe",
+            });
+          } else {
+            console.log(response.data["@error"]);
+            alert(response.data["@error"]);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
         });
-        console.log(users_communities);
-
+    } else {
+      userCommunityService
+        .subscribeCommunity(this.props.match.params.id)
+        .then((response) => {
+          console.log(response.data);
+          if (response.data["@success"] === "True") {
+            this.setState({
+              subscribed: true,
+              variant: "outlined",
+              text: "Unsubscribe",
+            });
+          } else {
+            console.log(response.data["@error"]);
+            alert(response.data["@error"]);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
+  }
 
-    checkSubscribeStatus() {
+  retrieveCommunity(id) {
+    UserCommunityService.getCommunityById(id)
+      .then((response) => {
+        this.setState({
+          communityTitle: response.data["@return"]["communityTitle"],
+          description: response.data["@return"]["description"],
+          creator: response.data["@return"]["createdBy"]["username"],
+          subscribers: response.data["@return"]["subscribers"],
+          posts: response.data["@return"]["posts"],
+          id: id,
+        });
+        this.checkSubscribeStatus();
         const myUser = AuthService.getCurrentUser();
-        if (this.state.subscribers.some(user => user.id === myUser.id)) {
-            /* vendors contains the element we're looking for */
-            console.log('subscribed')
-            this.setState({
-                subscribed: true,
-                variant: 'outlined',
-                text: 'Unsubscribe'
-            });
-        } else {
-            this.setState({
-                subscribed: false,
-                variant: 'contained',
-                text: 'Subscribe'
-            })
-            console.log('not subscribed yet')
+        if (response.data["@return"].createdBy.id === myUser.id) {
+          this.setState({ owner: true });
         }
-    }
+        console.log(response.data["@return"]);
+      })
+      .catch((e) => {
+        console.log(e.toString());
+      });
+  }
 
-    subscribe() {
-        console.log(this.state)
-        if (this.state.subscribed) {
-            userCommunityService.unsubscribeCommunity(this.props.match.params.id).then(response => {
-                console.log(response)
-                if (response.data['@success'] === "True") {
-                    this.setState({
-                        subscribed: false,
-                        variant: 'contained',
-                        text: 'Subscribe'
-                    })
-                } else {
-                    console.log(response.data['@error'])
-                    alert(response.data['@error'])
-                }
-            }).catch(error => {
-                console.log(error);
-            });
-        } else {
-            userCommunityService.subscribeCommunity(this.props.match.params.id).then(response => {
-                console.log(response.data)
-                if (response.data['@success'] === "True") {
-                    this.setState({
-                        subscribed: true,
-                        variant: 'outlined',
-                        text: 'Unsubscribe'
-                    });
-                } else {
-                    console.log(response.data['@error'])
-                    alert(response.data['@error'])
-                }
-            }).catch(error => {
-                console.log(error);
-            });
-        }
-    }
 
-    retrieveCommunity(id) {
-        UserCommunityService.getCommunityById(id)
-            .then(response => {
-                this.setState({
-                    communityTitle: response.data['@return']["communityTitle"],
-                    description: response.data['@return']["description"],
-                    creator: response.data['@return']["createdBy"]["username"],
-                    subscribers: response.data['@return']["subscribers"],
-                    posts: response.data['@return']["posts"],
-                    id: id
-                });
-                this.checkSubscribeStatus()
-                const myUser = AuthService.getCurrentUser();
-                if (response.data['@return'].createdBy.id === myUser.id) {
-                    this.setState({owner: true})
-                }
-                console.log(response.data['@return'])
-            })
-            .catch(e => {
-                console.log(e.toString());
-            });
-    }
+  render() {
+    const {
+      posts,
+      owner,
+      creator,
+      description,
+      communityTitle,
+      variant,
+      text,
+      actions,
+    } = this.state;
+    return (
+      <>
+        <Profilebar />
+        <Container maxWidth="md">
+          <Toolbar
+            sx={{ height: "100px", borderBottom: 1, borderColor: "divider" }}
+          >
+            <Avatar
+              alt={communityTitle}
+              src="https://picsum.photos/200/200"
+              sx={{ width: 100, height: 100 }}
+            />
+            <div style={{ marginLeft: 10 }}>
+              <Button
+                id="subscribe"
+                size="Large"
+                variant={variant}
+                onClick={this.subscribe}
+              >
+                {text}
+              </Button>
+            </div>
+            <Typography
+              component="h2"
+              variant="h5"
+              color="inherit"
+              align="center"
+              sx={{ flex: 1 }}
+            >
+              {communityTitle}
+            </Typography>
+          </Toolbar>
+          <Grid container direction="row">
 
-    render() {
-        const {posts, owner, creator, description, communityTitle, variant, text, actions} = this.state
-        return (
-            <>
-            <Profilebar/>
-            <Container maxWidth="md">
-                <Toolbar sx={{height: "100px", borderBottom: 1, borderColor: 'divider'}}>
-                    <Avatar alt={communityTitle} src="https://picsum.photos/200/200" sx={{width: 100, height: 100}}/>
-                    <div style={{marginLeft: 10}}>
-                        <Button id="subscribe" size="Large" variant={variant} onClick={this.subscribe}>{text}</Button>
-                    </div>
-                    <Typography
-                        component="h2"
-                        variant="h5"
-                        color="inherit"
-                        align="center"
-                        sx={{flex: 1}}
-                    >
-                        {communityTitle}
-                    </Typography>
-                </Toolbar>
-                <Grid container direction="row">
-                    <CommunityPosts description="something" posts={posts}/>
-                    <CommunityAbout description={description} moderators={creator}
-                                    tags="soccer football "/>
-                </Grid>
-                <BasicSpeedDial owner={owner} communityId={this.props.match.params.id}/>
-            </Container>
-            </>
-        );
-    }
-
+            <CommunityPosts description="something" posts={posts} />
+            <CommunityAbout
+              description={description}
+              moderators={creator}
+              tags="soccer football "
+            />
+          </Grid>
+          <BasicSpeedDial
+            owner={owner}
+            communityId={this.props.match.params.id}
+          />
+        </Container>
+      </>
+    );
+  }
 }
 
 export default withRouter(Community);
