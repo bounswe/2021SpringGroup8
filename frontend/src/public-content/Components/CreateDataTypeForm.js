c561aa1c7215a0d1108a7041483a735bf0760df8
Emre <muhammedemre.demir@boun.edu.tr>
2021-12-28 13:04:58 +0300

    Frontend / Dynamic Data Type Creation is Implemented The static data type creation is changed to dynamic so admins can add as many fields as they want now. Following are done: 1- New Data Type Creation form is created with the name of CreateDataTypeForm.js 2- This form is changed with the old form in the CreateDataType.js page

---
+++ frontend/src/public-content/Components/CreateDataTypeForm.js
@@ -0,0 +1,147 @@
+import React, {useState} from "react";
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField'
+import {makeStyles} from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton";
+import RemoveIcon from "@material-ui/icons/Remove"
+import AddIcon from "@material-ui/icons/Add"
+import {InputLabel, MenuItem, Select} from "@material-ui/core";
+import {useHistory} from "react-router-dom";
+import userCommunityService from "../../services/user-community.service";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            align: "center"
+        },
+        button: {
+            margin: theme.spacing(1),
+
+        },
+    },
+    fieldGroup: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: theme.spacing(1)
+    }
+
+}))
+
+export default function CreateDataTypeFrom(props) {
+    const {communityId} = props;
+    const history = useHistory();
+
+    const classes = useStyles()
+    const [inputFields, setInputFields] = useState([
+        {
+            fieldName: '',
+            fieldType: '',
+        },
+    ]);
+    const [typeName, setTypeName] = useState("");
+
+    function handleChangeInput(index, event) {
+        const values = [...inputFields];
+        values[index][event.target.name] = event.target.value;
+        setInputFields(values);
+        return undefined;
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let json_object = {}
+        inputFields.map((inputGroup) => {
+            json_object[inputGroup["fieldName"]] = inputGroup["fieldType"]
+        });
+        console.log(communityId, json_object, typeName)
+        userCommunityService.createDataTypeForCommunity(communityId, json_object, typeName).then(response => {
+            console.log(response)
+            if(response.data['@success'] === "True"){
+                alert(JSON.stringify({"message": "successfully created data type"}, null, 2));
+                history.push('/community/' + communityId);
+            }
+            alert(JSON.stringify({"message": response.data['@error'] }, null, 2));
+        }).catch(error => {
+            console.log(error)
+        })
+
+        history.push('/community/' + communityId);
+
+    };
+
+    const handleAddFields = () => {
+        setInputFields([...inputFields, {fieldName: '', fieldType: '',}])
+    };
+
+    const handleRemoveFields = (index) => {
+        const values = [...inputFields];
+        values.splice(index, 1);
+        setInputFields(values);
+    };
+
+    function handleChangeTypeName(event) {
+        setTypeName(event.target.value);
+        return undefined;
+    }
+
+    return (
+        <Container maxWidth={"xl"}>
+            <h3 align={"center"}> Create New Data Type</h3>
+            <form className={classes.root} onSubmit={handleSubmit}>
+                <div className={classes.fieldGroup}>
+                    <TextField
+                        name="typeName"
+                        label="Data Type Name"
+                        variant="outlined"
+                        value={typeName}
+                        onChange={event => handleChangeTypeName(event)}
+                    />
+                </div>
+                {inputFields.map((inputField, index) => (
+                    <div key={index} className={classes.fieldGroup}>
+                        <TextField
+                            name="fieldName"
+                            label="Field Name"
+                            variant="outlined"
+                            value={inputField.fieldName}
+                            onChange={event => handleChangeInput(index, event)}
+                        />
+                        <Select
+                            name="fieldType"
+                            id="demo-simple-select"
+                            value={inputField.fieldType}
+                            label="Field Type"
+                            onChange={event => handleChangeInput(index, event)}
+                        >
+                            <MenuItem value={"str"}>String</MenuItem>
+                            <MenuItem value={"int"}>Integer</MenuItem>
+                            <MenuItem value={"location"}>Location</MenuItem>
+                        </Select>
+                        {/*
+                        <TextField
+                            name="fieldType"
+                            label="Field Type"
+                            variant="filled"
+                            value={inputField.fieldType}
+                            onChange={event => handleChangeInput(index, event)}
+                        />
+                        */}
+                        <IconButton onClick={() => handleRemoveFields(index)}>
+                            <RemoveIcon/>
+                        </IconButton>
+
+                        <IconButton onClick={() => handleAddFields()}>
+                            <AddIcon/>
+                        </IconButton>
+                    </div>
+                ))}
+                <Button variant="contained" color="primary" className={classes.button}
+                        onClick={handleSubmit}>Create</Button>
+            </form>
+        </Container>
+    );
+}
