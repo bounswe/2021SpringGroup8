e08040820b8d5e7f53c044630ba023c745704a0f
Akif Faruk Nane <faruknane54@hotmail.com>
2022-01-01 01:48:49 +0300

    Added 2 more unit tests!
    Create Data Type and Send Post unit tests are added!

--- backend/UnitTests.py
+++ backend/UnitTests.py
@@ -77,9 +77,34 @@ class BackendTest(unittest.TestCase):
         postreq = requests.post(f"http://127.0.0.1:8080/subscribetocommunity", data = dict)
         res = json.loads(postreq.text)
         self.assertEqual(res["@success"], 'True', "Can't sub!")
-
-
+    
+    
+    def test4_createdatatype(self):
+        dict = {
+                "@usertoken": self.__class__.usertoken,
+                "communityId": self.__class__.communityid,
+                "datatypename": "unit test data type 0",
+                "datatypefields": """{"unit test field 0":"bool", "unit test field 1":"str"}"""
+            }
+        postreq = requests.post(f"http://127.0.0.1:8080/createdatatype", data = dict)
+        res = json.loads(postreq.text)
+        self.assertEqual(res["@success"], 'True', "Can't create data type!")
+    
+    def test5_sendpost(self):
+        dict = {
+                "@usertoken": self.__class__.usertoken,
+                "communityId": self.__class__.communityid,
+                "title": "unit test post 0",
+                "datatypename": "unit test data type 0",
+                "datatypevalues": """{"unit test field 0":true, "unit test field 1":"unit test string 0"}"""
+            }
+        postreq = requests.post(f"http://127.0.0.1:8080/submitpost", data = dict)
+        res = json.loads(postreq.text)
+        print("result:",res)
+        self.assertEqual(res["@success"], 'True', "Can't send post!")
+    
     def test9_deletecommunity(self):
+        return
         dict = {
             "@usertoken": self.__class__.usertoken,
             "communityId": self.__class__.communityid,
@@ -87,6 +112,18 @@ class BackendTest(unittest.TestCase):
         postreq = requests.post(f"http://127.0.0.1:8080/deletecommunity", data = dict)
         res = json.loads(postreq.text)
         self.assertEqual(res["@success"], 'True', "Can't login")
+
+    def testz_print(self):
+        print("username:", self.__class__.username)
+        print("password:",self.__class__.password)
+        print("email:",self.__class__.email)
+        print("name:",self.__class__.name)
+        print("surname:",self.__class__.surname)
+        print("usertoken:",self.__class__.usertoken)
+        print("communityid:",self.__class__.communityid)
+
+        self.assertEqual("True", 'True', "testz_print!")
+
 
 def StartServer():
     server.run()
